1a0dfac09dfcb6db04e181f4b3396149004f9e4f
howardmurdock-oss <howardmurdock-oss@users.noreply.github.com>
2026-09-23 13:29:57 -0400

    Release v1.4.9 - automated multi-platform build

--- lib/services/quest_service.dart
+++ lib/services/quest_service.dart
@@ -12,7 +12,7 @@ import 'order_engine.dart';
 import 'sync_service.dart';
 
 class QuestService extends ChangeNotifier {
-  static const String appCurrentBuildVersion = '1.4.8';
+  static const String appCurrentBuildVersion = '1.4.9';
 
   // Valid Patreon Unlock Code hashes (stored securely as SHA-256 digests)
   // Included default codes: 'PATREON-VIP', 'QUESTS-2026', 'DIRECTIVE-CHAIN', 'PATREON-SUPPORTER', 'QUEST'
